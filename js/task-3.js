0f015274ec0068bb4d242e7039121b8b269c3a42
vovangrk <vovangrk@ukr.net>
2026-03-20 21:05:29 +0200

    hw 2

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,17 @@
-function getElementWidth(content, padding, border) {
-    const a = parseFloat(content);
-    const b = parseFloat(padding);
-    const c = parseFloat(border);
-    return a + b * 2 + c * 2;
-   
-    
+function checkForSpam(message) {
+    message = message.toLowerCase();
+    if (message.includes('spam') || message.includes('sale'))
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
